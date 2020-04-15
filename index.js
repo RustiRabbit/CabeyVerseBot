32ab0854ab9694dde789677575236ede8c6bd3e7
Oscar Stickland <oscarstickland@gmail.com>
2020-04-15 13:15:26 +1000

    Added TempBan & command help

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 
 // Channels
 const CabeyGangRole = process.env.CABEY_GANG_ROLE;
-
 const logsChannel = process.env.LOGS_CHANNEL;
 const purgRoleName = process.env.PURG_ROLE;
 const tempBanRoleName = process.env.BAN_ROLE;
@@ -20,6 +19,8 @@ client.login(process.env.TOKEN);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("!commands for help"); 
+
 })
 
 client.on("message", msg => {
@@ -118,7 +119,55 @@ client.on("message", msg => {
         } else {
             msg.channel.send("You do not have the correct perms");
         }
-    } 
+    } else if (msg.content.startsWith("!tempban")) {
+        var member = msg.mentions.members.first();
+
+        if(checkPerms(msg) == true) {
+            member.roles.member.roles.set([tempBanRoleName]).then( () => {
+                msg.channel.send("`" + getName(member) + "` has been temp banned");
+                const channel = msg.client.channels.resolve(logsChannel);
+                const embed = new Discord.MessageEmbed();
+                embed.setTitle("User was given a temp ban");
+                embed.setColor(0xff0000);
+                embed.setDescription(getName(msg.member) + " temp banned " + getName(member));
+                channel.send(embed)
+                console.log(getName(msg.member) + " temp banned " + getName(member))
+    
+            }).catch( (error)=> {
+                console.log(error);
+                msg.channel.send("Error (ask RustiRabbit)");
+            }); 
+        } else {
+            msg.channel.send("You do not have the correct perms");
+        }
+    } else if (msg.content.startsWith("!untempban")) {
+        var member = msg.mentions.members.first();
+        
+        if(checkPerms(msg) == true) {
+            member.roles.member.roles.remove([tempBanRoleName]).then( () => {
+                member.roles.member.roles.set([CabeyGangRole]);
+                msg.channel.send("`" + getName(member) + "` has been un-temp banned");
+                const channel = msg.client.channels.resolve(logsChannel);
+                const embed = new Discord.MessageEmbed();
+                embed.setTitle("User was un-temp banned");
+                embed.setColor(0xff0000);
+                embed.setDescription(getName(msg.member) + " un-temp banned " + getName(member));
+                channel.send(embed)
+                console.log(getName(msg.member) + " un-temp banned " + getName(member))
+            }).catch( (error) => {
+                msg.channel.send("Error (ask RustiRabbit)");
+                console.log(error);
+            })
+        } else {
+            msg.channel.send("You do not have the correct perms");
+        }
+    } else if (msg.content.startsWith("!commands")) {
+        if (checkPerms(msg)) {
+            msg.author.send("**Commands**\n!kick <user> - Kicks a user\n!ban <user> Bans a user\n!mute <user> Puts the user in purg\n!unmute <user> brings a user out of purg\n!tempban <user> places a user in tempban\n!untempban <user> brings a user out of temp ban");
+        } else {
+            msg.author.send("Well Well Well. It appears that you dont have the correct permissions to use the bot so why would I tell you what the bot can do when your a bad boy/girl?")
+        }
+    }
 })
 
 function checkPerms(msg) {
